1596e429a20d6931b968ae0849d82596e3ec3848
Kristina Riemer <kristina.riemer@weecology.org>
2015-10-23 11:52:36 -0600

    Finish original shell of code for all objectives

--- code/raster_module_code.R
+++ code/raster_module_code.R
@@ -53,18 +53,47 @@ plot(coord_crop) #Compare with CHM raster, should have different x-axis ranges
 
 # Challenge: play with resolution (i.e., pixel numbers)
 
-# TODO: reprojection of a single-band raster, ask others? 
+# TODO: introduce extract function w/ single-band raster using an arbitrary x,y coordinate
+# TODO: reprojection of a single-band raster, from/to 18 to 19
 # TODO: summarizing multiple pixels into one value
 # TODO: do raster math on single raster
 
 
 ### Objective 4: working with multi-band RGB rasters
-# File: HARV_RBG_ortho
-# Tutorial: Spatio-temporal
-# Functions: stack, plotRGB
-# Challenge: calculate NDVI
+
+# Read in multi-band raster with raster function, the default is the first band
+RGB_band1 <- raster("~/Documents/Graduate_School/Workshops/GST_hackathon/1_WorkshopData/NEON_RemoteSensing/HARV/HARV_RGB_Ortho.tif")
+plot(RGB_band1) #Point out dimension, CRS, and values attributes, but esp. band
+RGB_band1
+
+# Can specify which band you want to read in
+RGB_band2 <- raster("~/Documents/Graduate_School/Workshops/GST_hackathon/1_WorkshopData/NEON_RemoteSensing/HARV/HARV_RGB_Ortho.tif", band = 2
+plot(RGB_band2)
+RGB_band2
+
+# Use stack function to read in all bands
+RGB <- stack("~/Documents/Graduate_School/Workshops/GST_hackathon/1_WorkshopData/NEON_RemoteSensing/HARV/HARV_RGB_Ortho.tif")
+RGB #Look at dimensions, CRS, and min/max
+#Metadata file? 
+plot(RGB)
+
+# The plotRGB function plots all bands together, is an image like our eyes would see
+plotRGB(RGB, r = 1, g = 2, b = 3)
+
+#The black bands on the edges are zeros but should be NAs, might want to show 
+#them how to turn them into NAs? 
+
+# TODO: Challenge: calculate NDVI, but would need an infrared band to do this
 
 ### Objective 5: working with multi-band time series rasters
-# File: HARV/2011/NDVI
-# Tutorial: Spatio-temporal
-# Functions: plot (raster library)
+
+# Getting names of all NDVI files
+NDVI_path <- "~/Documents/Graduate_School/Workshops/GST_hackathon/1_WorkshopData/Landsat_NDVI/HARV/2011/ndvi"
+all_NDVI <- list.files(NDVI_path, full.names = TRUE, pattern = ".tif$")
+
+# Opening those files and plotting them
+NDVI_stack <- stack(all_NDVI)
+plot(NDVI_stack, zlim = c(1500, 10000), nc = 3)
+hist(NDVI_stack, xlim = c(1500, 10000))
+
+# TODO: Challenge: two of the times have weird values because of clouds, have them figure that out
