3124ef61cd187cbe1ace198d3291759d3f585030
lwasser <lwasser@neoninc.org>
2015-12-17 10:50:38 -0800

    rebuild and final edits to lesson 00

--- 00-Raster-Structure.R
+++ 00-Raster-Structure.R
@@ -9,7 +9,7 @@ library(rgdal)
 DSM_HARV <- raster("NEON_RemoteSensing/HARV/DSM/HARV_dsmCrop.tif")
 
 # code output here - DEM rendered on the screen
-plot(DSM_HARV, main="Continuous Elevation Map\nHarvard Forest")
+plot(DSM_HARV, main="Continuous Elevation Map\n NEON Harvard Forest Field Site")
 
 
 ## ----classified-elevation-map, echo=FALSE--------------------------------
@@ -23,7 +23,7 @@ brk <- c(250,350, 380,500)
 # Expand right side of clipping rect to make room for the legend
 par(xpd = FALSE,mar=c(5.1, 4.1, 4.1, 4.5))
 #DEM with a custom legend
-plot(DSM_HARV, col=col, breaks=brk, main="Classified Elevation Map\nHarvard Forest",legend = FALSE)
+plot(DSM_HARV, col=col, breaks=brk, main="Classified Elevation Map\nNEON Harvard Forest Field Site",legend = FALSE)
 #turn xpd back on to force the legend to fit next to the plot.
 par(xpd = TRUE)
 #add a legend - but make it appear outside of the plot
@@ -46,7 +46,7 @@ DSM_HARV <- raster("NEON_RemoteSensing/HARV/DSM/HARV_dsmCrop.tif")
 # View raster structure
 DSM_HARV 
 
-#simple plot of the raster
+#plot raster
 #note \n in the title forces a line break in the title
 plot(DSM_HARV, 
      main="NEON Digital Surface Model\nHarvard Forest")
@@ -60,8 +60,9 @@ crs(DSM_HARV)
 myCRS <- crs(DSM_HARV)
 myCRS
 
-## ----resolutio-untis-----------------------------------------------------
-DSM_HARV@data
+
+## ----resolution-units----------------------------------------------------
+crs(DSM_HARV)
 
 ## ----set-min-max---------------------------------------------------------
 
@@ -75,7 +76,7 @@ minValue(DSM_HARV)
 maxValue(DSM_HARV)
 
 
-## ----demonstrate-no-data-blaco, echo=FALSE-------------------------------
+## ----demonstrate-no-data-black, echo=FALSE-------------------------------
 #demonstration code below - not being taught - just demonstrating no data values
 # Use stack function to read in all bands
 RGB_stack <- stack("NEON_RemoteSensing/HARV/HARV_RGB_Ortho.tif")
@@ -99,14 +100,14 @@ newRGBImage <- calc(RGB_stack, f)
 par(col.axis="white",col.lab="white",tck=0)
 # Create an RGB image from the raster stack
 plotRGB(newRGBImage, r = 1, g = 2, b = 3,
-        axes=TRUE, main="Raster With No Data Values\nNodata= NA")
+        axes=TRUE, main="Raster With No Data Values\nNoDataValue= NA")
  
 
 ## ----view-raster-histogram-----------------------------------------------
 
 #view histogram of data
 hist(DSM_HARV,
-     main="Digital Surface Model - Range of Values\n NEON Harvard Forest",
+     main="Distribution of Digital Surface Model Values\n Histogram Default: 100,000 pixels\n NEON Harvard Forest",
      xlab="DSM Elevation Value (m)",
      ylab="Frequency",
      col="wheat")
@@ -120,7 +121,7 @@ ncell(DSM_HARV)
 #create histogram that includes with all pixel values in the raster
 hist(DSM_HARV, 
      maxpixels=ncell(DSM_HARV),
-     main="Digital Surface Model - Range of Values\n All Pixel Values Included\n NEON Harvard Forest",
+     main="Distribution of Digital Surface Model Values\n All Pixel Values Included\n NEON Harvard Forest",
      xlab="DSM Elevation Value (m)",
      ylab="Frequency",
      col="wheat4")
@@ -143,7 +144,7 @@ GDALinfo("NEON_RemoteSensing/HARV/DSM/HARV_dsmCrop.tif")
 ## 
 ## #ANSWERS ###
 ## #1. If this file has the same CRS as DSM_HARV?  Yes: UTM Zone 18, WGS84, meters.
-## #2. What format NoData values take?  -9999
+## #2. What format `NoDataValues` take?  -9999
 ## #3. The resolution of the raster data? 1x1
 ## #4. How large a 5x5 pixel area would be? 5mx5m How? We are given resolution of
 ## #1x1 and units in meters, therefore rolution of 5x5 means 5x5m.
