02b38fd048afa0607bf727a6b592cafa0db7e760
François Michonneau <francois.michonneau@gmail.com>
2018-04-16 17:48:19 -0400

    update build script

--- build_lesson.R
+++ build_lesson.R
@@ -3,10 +3,12 @@ BuildLesson <- R6::R6Class(
     "BuildLesson", inherit = TicStep,
     public = list(
         run = function() {
-        build_status <- system("make site")
-        if (build_status > 0)
-            stop("Error during building process")
-    })
+            build_status <- system("make lesson-md")
+            if (build_status > 0)
+                stop("Error during building process")
+            system("mkdir  _rendered")
+            system("cp -r \`ls -A | grep -v '.git' | grep -v '_rendered' | grep -v '_site'\` _rendered")
+        })
 )
 
 build_lesson <- function() {
