0143bcaf96f1238bd0de053bbd68364003c9687b
François Michonneau <francois.michonneau@gmail.com>
2018-08-22 09:57:16 -0400

    update deploy scripts

--- build_lesson.R
+++ build_lesson.R
@@ -6,6 +6,7 @@ BuildLesson <- R6::R6Class(
           build_status <- system("make clean-rmd; make lesson-md")
           if (build_status > 0)
             stop("Error during building process")
+
           system("mkdir  _rendered")
           system("cp -r \`ls -A | grep -v '.git' | grep -v '_rendered' | grep -v '_site'\` _rendered")
         })
