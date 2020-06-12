f6dde0c5724b1b3d5835bbe6c8b03a31c475d2c3
Sarah Graves <sjgraves142@gmail.com>
2020-06-12 12:36:55 -0500

    Referencing new Figshare data subset

--- setup.R
+++ setup.R
@@ -1,31 +1,10 @@
 ## file structure
 
-if (! file.exists("data/NEON-DS-Site-Layout-Files")) {
-    dest <- tempfile()
-    download.file("https://ndownloader.figshare.com/files/3708751", dest,
-                  mode = "wb")
-    unzip(dest, exdir = "data")
-}
-
-if (! file.exists("data/NEON-DS-Airborne-Remote-Sensing")) {
-    dest <- tempfile()
-    download.file("https://ndownloader.figshare.com/files/3701578", dest,
-                  mode = "wb")
-    unzip(dest, exdir = "data")
-}
-
-if (! file.exists("data/NEON-DS-Met-Time-Series")) {
-    dest <- tempfile()
-    download.file("https://ndownloader.figshare.com/files/3701572", dest,
-                  mode = "wb")
-    unzip(dest, exdir = "data")
-}
-
-if (! file.exists("data/NEON-DS-Landsat-NDVI")) {
-    dest <- tempfile()
-    download.file("https://ndownloader.figshare.com/files/4933582", dest,
-                  mode = "wb")
-    unzip(dest, exdir = "data")
+if (! file.exists("data/Geospatial_data")) {
+  dest <- tempfile()
+  download.file("https://ndownloader.figshare.com/files/23104040", dest,
+                mode = "wb")
+  unzip(dest, exdir = "data")
 }
 
 if (! file.exists("data/Global/Boundaries/ne_110m_graticules_all")) {
