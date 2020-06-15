06341693cbb52d69c62abffbef4ac1997399929d
mkamenet3 <maria.kamenetsky@gmail.com>
2020-06-15 14:28:30 -0500

    updated all Rmd files and setup script

--- setup.R
+++ setup.R
@@ -1,43 +1,9 @@
 ## file structure
+## file structure
 
-if (! file.exists("data/NEON-DS-Site-Layout-Files")) {
+if (! file.exists("data/raster") | ! file.exists("data/vector")) {
     dest <- tempfile()
-    download.file("https://ndownloader.figshare.com/files/3708751", dest,
+    download.file("https://ndownloader.figshare.com/files/23104040", dest,
                   mode = "wb")
     unzip(dest, exdir = "data")
-}
-
-if (! file.exists("data/NEON-DS-Airborne-Remote-Sensing")) {
-    dest <- tempfile()
-    download.file("https://ndownloader.figshare.com/files/3701578", dest,
-                  mode = "wb")
-    unzip(dest, exdir = "data")
-}
-
-if (! file.exists("data/NEON-DS-Met-Time-Series")) {
-    dest <- tempfile()
-    download.file("https://ndownloader.figshare.com/files/3701572", dest,
-                  mode = "wb")
-    unzip(dest, exdir = "data")
-}
-
-if (! file.exists("data/NEON-DS-Landsat-NDVI")) {
-    dest <- tempfile()
-    download.file("https://ndownloader.figshare.com/files/4933582", dest,
-                  mode = "wb")
-    unzip(dest, exdir = "data")
-}
-
-if (! file.exists("data/Global/Boundaries/ne_110m_graticules_all")) {
-    dest <- tempfile()
-    download.file("http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/physical/ne_110m_graticules_all.zip",
-                  dest, mode = "wb")
-    unzip(dest, exdir = "data/Global/Boundaries/ne_110m_graticules_all")
-}
-
-if (! file.exists("data/Global/Boundaries/ne_110m_land")) {
-    dest <- tempfile()
-    download.file("http://www.naturalearthdata.com/http//www.naturalearthdata.com/download/110m/physical/ne_110m_land.zip",
-                  dest, mode = "wb")
-    unzip(dest, exdir = "data/Global/Boundaries/ne_110m_land")
 }
